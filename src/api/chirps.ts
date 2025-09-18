19e2b961996c2bffb53356f567420631f7d28e3f
Yssengrim <eisengrin@gmail.com>
2025-09-18 16:23:22 -0500

    CH4 JSON commit

---
+++ src/api/chirps.ts
@@ -0,0 +1,34 @@
+import type { Request, Response } from "express";
+
+import { respondWithJSON, respondWithError } from "./json.js";
+
+export async function handlerChirpsValidate(req: Request, res: Response) {
+  type parameters = {
+    body: string;
+  };
+
+  const params: parameters = req.body;
+
+  const maxChirpLength = 140;
+  if (params.body.length > maxChirpLength) {
+    respondWithError(res, 400, "Chirp is too long");
+    return;
+  }
+
+  const words = params.body.split(" ");
+
+  const badWords = ["kerfuffle", "sharbert", "fornax"];
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const loweredWord = word.toLowerCase();
+    if (badWords.includes(loweredWord)) {
+      words[i] = "****";
+    }
+  }
+
+  const cleaned = words.join(" ");
+
+  respondWithJSON(res, 200, {
+    cleanedBody: cleaned,
+  });
+}
